abf781840d80c44c4cff334adcd6e486eefd03b9
Luca Cavallin <lucavallin@github.com>
2022-12-07 09:51:45 +0100

    remove unused files

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,7 +2,7 @@
   <header class="w-full py-4 border-b border-ink-200 bg-ink-400">
     <nav class="flex items-center justify-center flex-wrap">
       <nuxt-link to="/" class="flex items-center text-gray-700 font-bold">
-        <img src="~/assets/gfi-logo-white.svg" alt="Good First Issue" class="h-12" />
+        <img src="~/static/gfi-logo-white.svg" alt="Good First Issue" class="h-12" />
       </nuxt-link>
       <span v-if="activeTag" class="text-2xl cursor-pointer">
         <span class="font-normal ml-2 mr-1 text-slate">/</span>
