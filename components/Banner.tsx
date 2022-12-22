d61665a11756a50d5e47533fa18d2e70a75b80f3
Luca Cavallin <lucavallin@github.com>
2022-12-22 16:15:34 +0100

    add pre-commit hooks for linting

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,4 +1,4 @@
-import React, { Component, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 type BannerProps = {
   children: React.ReactNode
@@ -13,7 +13,9 @@ export const Banner = ({ children }: BannerProps) => {
 
   return (
     <header
-      className={`"w-full py-4 px-6 border-b border-ink-200 transition-background duration-1000 ${`bannerActive ? 'bg-robin' : 'bg-ink-300'`}"`}
+      className={`"w-full py-4 px-6 border-b border-ink-200 transition-background duration-1000 ${
+        active ? 'bg-robin' : 'bg-ink-300'
+      }"`}
     >
       <span className="flex items-center justify-center flex-wrap text-sm font-medium">
         <span className="md:flex items-center">{children}</span>
