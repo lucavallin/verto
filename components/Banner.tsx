ba4f85515a47c0e5d5ceeaa1bb77ce04f0fe50a2
Luca Cavallin <lucavallin@github.com>
2022-12-22 10:15:08 +0100

    reactify Banner.tsx and Layout.tsx

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,29 +1,23 @@
-import React, { Component } from "react"
+import React, { Component, useEffect, useState } from 'react'
 
-export default class ComponentAAAA extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      bannerActive: true
-    }
-  }
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({ bannerActive: false })
-    }, 2000)
-    this.setState({ bannerActive: false })
-  }
-  render() {
-    return (
-      <header
-        className={`"w-full py-4 px-6 border-b border-ink-200 transition-background duration-1000 ${`bannerActive ? 'bg-robin' : 'bg-ink-300'`}"`}
-      >
-        <span className="flex items-center justify-center flex-wrap text-sm font-medium">
-          <span className="md:flex items-center">
-            <slot></slot>
-          </span>
-        </span>
-      </header>
-    )
-  }
+type BannerProps = {
+  children: React.ReactNode
+}
+
+export const Banner = ({ children }: BannerProps) => {
+  const [active, setActive] = useState(true)
+  useEffect(() => {
+    const timer = setTimeout(() => setActive(false), 1000)
+    return () => clearTimeout(timer)
+  }, [])
+
+  return (
+    <header
+      className={`"w-full py-4 px-6 border-b border-ink-200 transition-background duration-1000 ${`bannerActive ? 'bg-robin' : 'bg-ink-300'`}"`}
+    >
+      <span className="flex items-center justify-center flex-wrap text-sm font-medium">
+        <span className="md:flex items-center">{children}</span>
+      </span>
+    </header>
+  )
 }
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,15 +4,14 @@ import Sidebar from './Sidebar'
 import Navbar from './Navbar'
 import { Inter } from '@next/font/google'
 
-// this must be moved to _app.tsx
+// TODO this must be moved to _app.tsx
 const inter = Inter()
 
 type LayoutProps = {
   children: React.ReactNode
 }
 
-// Easiest way to declare a Function Component; return type is inferred.
-const Layout = ({ children }: LayoutProps) => (
+export const Layout = ({ children }: LayoutProps) => (
   <div className="bg-ink-400 flex flex-col min-h-screen antialiased text-vanilla-300">
     <Navbar tag="tag"></Navbar>
     <main className={`flex flex-1 ${inter.className}`}>
