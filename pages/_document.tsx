bb41c0cba335f9579d47eac1a29b6f223ee8436b
Luca Cavallin <lucavallin@github.com>
2022-12-22 17:23:17 +0100

    fix meta headers

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,19 +10,32 @@ export default function Document() {
           name="description"
           content="Making your first open-source contribution is easier than you think. Good First Issue is a curated list of issues from popular open-source projects that you can fix easily. Start today!"
         />
+
+        <meta property="og:url" content="https://gfi.cavall.in" />
+        <meta property="og:type" content="website" />
         <meta
-          name="keywords"
-          content="good first issue, open source, github, beginner, pull requests, help wanted"
-        />
-        <meta
-          name="og:title"
+          property="og:title"
           content="Good First Issue: issues for your first open-source contribution"
         />
         <meta
-          name="og:description"
+          property="og:description"
           content="Making your first open-source contribution is easier than you think. Good First Issue is a curated list of issues from popular open-source projects that you can fix easily. Start today!"
         />
-        <meta name="og:image" content="/public/meta.jpg" />
+        <meta property="og:image" content="https://gfi.cavall.in/meta.jpg" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="gfi.cavall.in" />
+        <meta property="twitter:url" content="https://gfi.cavall.in" />
+        <meta
+          name="twitter:title"
+          content="Good First Issue: issues for your first open-source contribution"
+        />
+        <meta
+          name="twitter:description"
+          content="Making your first open-source contribution is easier than you think. Good First Issue is a curated list of issues from popular open-source projects that you can fix easily. Start today!"
+        />
+        <meta name="twitter:image" content="https://gfi.cavall.in/meta.jpg" />
+
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="UTF-8" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
