954bc1fb8679a169b3b7bfd0443ee990861fa2d5
Luca Cavallin <lucavallin@github.com>
2022-12-23 23:27:27 +0100

    fix tag shown in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,16 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React from "react";
+
+import { useAppContext } from "../pages/_app";
+import { Tag } from "../types";
 
 export const Navbar = () => {
-  const router = useRouter()
-  const { tag } = router.query
+  const router = useRouter();
+  const { tag: path } = router.query;
+  const { tags } = useAppContext();
+  const tag = tags.find((tag: Tag) => tag.id === path);
 
   return (
     <header className="w-full py-4 border-b border-ink-200 bg-ink-400">
@@ -13,13 +18,13 @@ export const Navbar = () => {
         <Link href="/" className="flex items-center text-gray-700 font-bold">
           <Image src="/gfi.png" alt="Good First Issue" className="h-12" width={355} height={51} />
         </Link>
-        {tag ? (
+        {path ? (
           <span className="text-2xl cursor-pointer">
             <span className="font-normal ml-2 mr-1 text-slate">/</span>
-            <span className="font-semibold text-juniper">{tag}</span>
+            <span className="font-semibold text-juniper">{tag.display}</span>
           </span>
         ) : null}
       </nav>
     </header>
-  )
-}
+  );
+};
