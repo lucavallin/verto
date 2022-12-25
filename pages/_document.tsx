48411b2e64d7a88bfb39ab904e9f6bd1c7a803b4
Luca Cavallin <lucavallin@github.com>
2022-12-25 22:04:22 +0100

    update docs, description, analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,5 @@
 import { Head, Html, Main, NextScript } from "next/document";
 import Script from "next/script";
-import { GoogleAnalytics } from "nextjs-google-analytics";
 
 export default function Document() {
   return (
