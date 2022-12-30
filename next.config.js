aff95aaa3aa38ff3a1b370b9cdb37db7b4f40fed
Luca Cavallin <lucavallin@github.com>
2022-12-30 19:51:41 +0100

    remove headers, they are not used

--- next.config.js
+++ next.config.js
@@ -4,18 +4,6 @@ const nextConfig = {
   reactStrictMode: true,
   productionBrowserSourceMaps: true,
   trailingSlash: true,
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      '/': { page: '/' },
-      '/language': { page: '/language' },
-      '/p/hello-nextjs': { page: '/post', query: { title: 'hello-nextjs' } },
-      '/p/learn-nextjs': { page: '/post', query: { title: 'learn-nextjs' } },
-      '/p/deploy-nextjs': { page: '/post', query: { title: 'deploy-nextjs' } },
-    }
-  },
 }
 
 module.exports = nextConfig
