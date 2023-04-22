4dd2feaa63f947a2fc8f41fe58c3243c2337a50f
Mario Adisurya <mario.adisurya@gmail.com>
2023-04-22 18:54:29 +1200

    feat(topic): initial attempt with `getStaticPaths`

--- pages/topic/[tag].tsx
+++ pages/topic/[tag].tsx
@@ -1,17 +1,36 @@
+import { GetStaticPaths, GetStaticProps } from "next";
 import Head from "next/head";
-import { useRouter } from "next/router";
+import { ParsedUrlQuery } from "querystring";
 
 import { RepositoryList } from "../../components/RepositoryList";
 import { useAppContext } from "../_app";
 
-export default function Topic() {
+interface Params extends ParsedUrlQuery {
+  tag: string | undefined;
+}
+
+type TopicProps = {
+  tag: Params["tag"];
+};
+
+export const getStaticPaths: GetStaticPaths<Params> = async () => {
+  return {
+    paths: [],
+    fallback: true
+  };
+};
+
+export const getStaticProps: GetStaticProps<TopicProps, Params> = async ({ params = {} }) => {
+  return {
+    props: { tag: params.tag }
+  };
+};
+
+export default function Topic({ tag }: TopicProps) {
   const { repositories, topics } = useAppContext();
 
-  const router = useRouter();
-  const { tag } = router.query;
-
-  const topic = topics.find((topic) => topic.id == tag);
-  const pageTitle = `First Issue | Topic ${topic?.display}`;
+  const topic = topics.find((topic) => topic.id === tag);
+  const pageTitle = `First Issue | Topic ${topic?.display ?? ""}`;
 
   return (
     <>
@@ -20,7 +39,7 @@ export default function Topic() {
       </Head>
       <RepositoryList
         repositories={repositories.filter((repository) =>
-          repository.topics?.some((topic) => topic.id == tag)
+          repository.topics?.some((topic) => topic.id === tag)
         )}
       />
     </>
