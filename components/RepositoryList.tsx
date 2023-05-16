b84857adc23714756b8644619901d614118418d6
Isaac Curtiss <ijcurtiss7@gmail.com>
2023-05-15 21:58:13 -0400

    spacing fix

--- components/RepositoryList.tsx
+++ components/RepositoryList.tsx
@@ -32,7 +32,7 @@ export const RepositoryList = ({ repositories }: RepositoryListProps) => {
           loader={<Loader />}
         >
           {repositories.slice(0, items).map((repository) => {
-            //NOTE - We sometimes get duplicate values back from GitHub API
+            // NOTE - We sometimes get duplicate values back from GitHub API
             // meaning we can't simply rely on the id as the key
             const key = `${repository.id}_${new Date().getTime()}_${Math.random()}`;
             return <RepositoryItem key={key} repository={repository} />;
