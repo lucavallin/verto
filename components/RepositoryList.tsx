bc94a68cfdb92b3070a87779492b863147f6318d
Isaac <43455920+isaacwashere@users.noreply.github.com>
2023-05-16 05:12:10 -0400

    fix/duplicate results from the sorting  (#114)
    * ids aren't unique when results have duplicates
    
    * use the date to make the key more unique
    
    * spacing fix
    
    * add space for readability

--- components/RepositoryList.tsx
+++ components/RepositoryList.tsx
@@ -31,9 +31,13 @@ export const RepositoryList = ({ repositories }: RepositoryListProps) => {
           hasMore={items < repositories.length}
           loader={<Loader />}
         >
-          {repositories.slice(0, items).map((repository) => (
-            <RepositoryItem key={repository.id} repository={repository} />
-          ))}
+          {repositories.slice(0, items).map((repository) => {
+            // NOTE - We sometimes get duplicate values back from GitHub API
+            // meaning we can't simply rely on the id as the key
+            const key = `${repository.id}_${new Date().getTime()}_${Math.random()}`;
+
+            return <RepositoryItem key={key} repository={repository} />;
+          })}
         </InfiniteScroll>
       </div>
     </main>
