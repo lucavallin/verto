5a855812887605dc4edb440b9587c20593c8ebaa
Luca Cavallin <lucavallin@users.noreply.github.com>
2023-10-01 11:30:19 +0200

    replace google analytics with plausible

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Head, Html, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 export default function Document() {
   return (
@@ -38,24 +37,7 @@ export default function Document() {
         <meta charSet="UTF-8" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
 
-        <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('consent', 'default', {
-            'ad_storage': 'denied',
-            'analytics_storage': 'denied'
-          });
-
-          gtag('config', ${process.env.NEXT_PUBLIC_GA_TRACKING_ID});
-        `}
-        </Script>
+        <script defer data-domain="firstissue.dev" src="https://plausible.io/js/script.js"></script>
       </Head>
       <body>
         <Main />
