8c02f5e5d0a56ffdc4ddacf3e76ac52fbf059301
Abhinav Pandey <abhinavxox@gmail.com>
2023-10-02 17:13:51 +0545

    Fix : Mobile view navigation improvements (#199)
    * Add : Added collapsible feature to Tags and Language Picker Section
    
    * Add : Added scroll to repository list feature
    
    * Fix : Fixed lint error
    
    * Fix : Fixed reiviewed comment issues
    
    ---------
    
    Co-authored-by: Luca Cavallin <14332663+lucavallin@users.noreply.github.com>

--- components/Picker/LanguagePicker.tsx
+++ components/Picker/LanguagePicker.tsx
@@ -1,3 +1,6 @@
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 import { CountableTag } from "../../types";
 import { SectionTitle } from "../SectionTitle";
 import { PickerItem } from "./PickerItem";
@@ -9,17 +12,38 @@ type LanguagePickerProps = {
 };
 
 export const LanguagePicker = ({ activeTagId, languages, onLanguagePage }: LanguagePickerProps) => {
+  const [isCollapsed, setIsCollapsed] = useState<boolean>(true);
+
+  const toggleCollapsible = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+
   return (
     <div className="pt-6">
-      <SectionTitle text="Browse by Language" />
-      <div>
+      <div
+        onClick={toggleCollapsible}
+        className={`flex cursor-pointer ${isCollapsed ? "sm:flex" : ""}`}
+      >
+        <SectionTitle text="Browse by Language" />
+        <FontAwesomeIcon
+          icon={faChevronDown}
+          className={`mx-2 mt-[3px] transform text-secondary transition-transform ${
+            isCollapsed ? "rotate-0" : "rotate-180"
+          } animate-fade-in duration-300 ease-in-out md:hidden`}
+        />
+      </div>
+      <div
+        className={`transition-max-height overflow-hidden duration-300 ease-in-out ${
+          isCollapsed ? "max-h-0" : "max-h-96"
+        } ${isCollapsed ? "sm:max-h-full" : ""}`}
+      >
         {languages.map((language) => {
           return (
             <PickerItem
               className={`group mx-1 my-1 inline-block rounded-sm border px-2 py-1 text-sm ${
                 onLanguagePage && language.id === activeTagId
                   ? "active-pill"
-                  : "border-secondary transition-all transition-all hover:border-primary hover:text-primary"
+                  : "border-secondary transition-all hover:border-primary hover:text-primary"
               }`}
               href={`/language/${language.id}`}
               key={language.id}
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,12 +8,21 @@ import { LinkButton } from "./Button/LinkButton";
 import { NewsletterSection } from "./NewsletterSection";
 import { LanguagePicker } from "./Picker/LanguagePicker";
 import { TagPicker } from "./Picker/TagPicker";
+import { useEffect } from "react";
 
 export const Sidebar = () => {
   const router = useRouter();
   const { languages, tags } = useAppData();
   const { tag: activeTagId } = router.query;
   const pageName = router.pathname.split("/")[1];
+  
+  useEffect(() => {
+    const scrollTarget = document.getElementById("repositories-list");
+    const isMobile = window.innerWidth <= 640; 
+    if (isMobile && (pageName === "language" || pageName === "tag")) {
+      scrollTarget?.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [router.asPath, pageName]);
 
   return (
     <section className="w-full flex-none px-6 font-sans text-light-300 md:max-w-sm">
