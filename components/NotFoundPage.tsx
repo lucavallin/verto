65130018544a625fe75728842f05f88e08a46739
vivek kumar <vk285090@gmail.com>
2023-10-04 04:19:22 +0530

    created new customize 404 page

--- components/NotFoundPage.tsx
+++ components/NotFoundPage.tsx
@@ -5,7 +5,7 @@ const NotFoundPage = () => {
     <div className="flex min-h-screen flex-col items-center  justify-center space-y-10 bg-dark-400 text-light-300 antialiased ">
       <h3 className="text-3xl font-bold text-primary lg:text-5xl">404😅</h3>
 
-      <h3 className="text-xl lg:text-3xl">Look Like you@apos;re lost</h3>
+      <h3 className="text-xl lg:text-3xl">Look Like you&apos;re lost</h3>
       <p className="text-secondary">the page you are looking for not available</p>
       <Link href="/" className="text-white border border-primary p-4 hover:bg-primary">
         Go back to home
