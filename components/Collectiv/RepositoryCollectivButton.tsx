2388a121e49c3225a2c21ee0594d9e48f88c7b27
tiwarishankar <tiwarishankar587@gmail.com>
2023-10-11 15:33:48 +0530

    resolve comments

--- components/Collectiv/RepositoryCollectivButton.tsx
+++ components/Collectiv/RepositoryCollectivButton.tsx
@@ -1,5 +1,3 @@
-import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Repository } from "../../types";
 
 type RepositoryCollectivButtonProps = {
@@ -17,12 +15,11 @@ export const RepositoryCollectivButton = ({
       href={`https://chat.collectivai.com/contribute?repo-name=${ownerAndRepoName}`}
       className="m-2 flex items-center  justify-center
            gap-2  rounded-lg border border-primary p-2 text-sm font-semibold
-          md:m-0 md:mx-2 md:gap-1 md:rounded-full  md:border-transparent md:bg-primary md:px-[14px] md:text-light-200"
+           md:m-0  md:rounded-full md:border-transparent md:bg-primary md:px-3"
       target="_blank"
       rel="noopener noreferrer"
     >
-      Get help from Collectiv
-      <FontAwesomeIcon icon={faArrowRight} className="ml-1  h-3 w-3" />
+      Solve with Collectiv
     </a>
   );
 };
