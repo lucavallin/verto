a843785644b747edef2f51e74d8908fb35cc36fa
Hasan Ahmed <18203044@iubat.edu>
2023-10-13 02:08:53 +0600

    188 improve UI design of repository items (#244)
    * fix(issue list): added min-widht at issue number span and made text-write work
    
    * fix:added extra width for issue number and added right margin with issue title if there's no comment on the issue
    
    * fix: Changed MetaData color and text alignment fixed for repo description
    
    * .idea file content removed from pr

--- components/Repository/IssueItem.tsx
+++ components/Repository/IssueItem.tsx
@@ -13,7 +13,7 @@ type IssueCommentNumProps = {
 
 const IssueCommentNum = ({ numIssues }: IssueCommentNumProps) => {
   return (
-    <div className="mt-1 flex w-10 flex-row items-center justify-end text-secondary">
+    <div className="mt-1 flex w-10 flex-row items-center justify-end text-grayish">
       <span className="mr-2 text-sm leading-snug">{numIssues}</span>
       <FontAwesomeIcon icon={faComment} />
     </div>
@@ -23,10 +23,10 @@ const IssueCommentNum = ({ numIssues }: IssueCommentNumProps) => {
 export const IssueItem = ({ issue }: IssueItemProps) => {
   return (
     <li key={issue.url} className="flex flex-row items-start justify-start py-1">
-      <span className="w-30 px-2 text-right leading-snug text-secondary">#{issue.number}</span>
+      <span className="min-w-[74px] pr-2 text-right leading-snug text-grayish">#{issue.number}</span>
       <div className="flex flex-auto flex-row items-start">
         <a
-          className="mr-4 block flex-auto break-all leading-snug transition-all hover:text-primary"
+          className={`block flex-auto leading-snug transition-all hover:text-primary ${issue.comments_count > 0 ? 'mr-4' : 'mr-12'}`}
           href={issue.url}
           rel="noopener noreferrer"
           target="_blank"
