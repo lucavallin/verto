0a4df4ec2496876276050cd5fe4204a8d12c3df9
Dina Basumatary <dins.basu@gmail.com>
2023-10-13 06:38:11 +1100

    Refactor data fetching and filtering logic in get.ts (#229)
    * refactor data/get.ts
    
    * simplify function processSource
    
    * introduced CountableLanguage and fixed references
    
    ---------
    
    Co-authored-by: Dina Basumatary <dnafication@users.noreply.github.com>

---
+++ data/index.ts
@@ -0,0 +1,43 @@
+import config from "../config.json";
+import { Data, Repository as RepositoryModel, Source as SourceModel } from "../types";
+import { getFilteredLanguages, getFilteredTags, processSource } from "./shared";
+import { generateAndWriteSiteMap, writeDataFile } from "./utils";
+
+const main = async () => {
+  console.log(
+    "⚠️ This command must be run from the root of the project directory with `npm run prebuild`"
+  );
+  try {
+    // Get data from all sources defined in config.json
+    const repositories = await (config as SourceModel[]).reduce<Promise<RepositoryModel[]>>(
+      async (repoData, source) => {
+        return repoData.then(async (repos) => {
+          const repositories = await processSource(source);
+          return [...repos, ...repositories];
+        });
+      },
+      Promise.resolve([])
+    );
+
+    // Get a list of distinct languages with counts for use with filtering in the UI
+    const filteredLanguages = getFilteredLanguages(repositories);
+
+    // Get a list of distinct tags with counts for use with filtering in the UI
+    const filteredTags = getFilteredTags(repositories);
+
+    const data: Data = {
+      // Sort the repositories randomly so that the list isn't always the same
+      repositories: repositories.sort(() => Math.random() - 0.5),
+      languages: filteredLanguages,
+      tags: filteredTags
+    };
+
+    await Promise.all([writeDataFile(data), generateAndWriteSiteMap(data)]);
+
+    console.log("Data generation complete.");
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+main();
