e5bcbd50b3015b4f8c80c7e16aad80b4e789e5b6
vivek kumar <vk285090@gmail.com>
2023-10-12 10:28:13 +0530

    successfully implemented Active Tag Button feature

--- components/Picker/LanguagePicker.tsx
+++ components/Picker/LanguagePicker.tsx
@@ -3,8 +3,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useState } from "react";
 import { CountableTag } from "../../types";
 import { SectionTitle } from "../SectionTitle";
+import ActiveTagButton from "./ActiveTagButton";
 import { PickerItem } from "./PickerItem";
-
 type LanguagePickerProps = {
   activeTagId: string | string[] | undefined;
   languages: CountableTag[];
@@ -13,9 +13,10 @@ type LanguagePickerProps = {
 
 export const LanguagePicker = ({ activeTagId, languages, onLanguagePage }: LanguagePickerProps) => {
   const [isCollapsed, setIsCollapsed] = useState<boolean>(true);
-
+  
   // Automatically collapse the sidebar after redirection
   useEffect(() => {
+    
     setIsCollapsed(true);
   }, [onLanguagePage, activeTagId]);
 
@@ -28,15 +29,16 @@ export const LanguagePicker = ({ activeTagId, languages, onLanguagePage }: Langu
     <div className="pt-6">
       <div
         onClick={toggleCollapsible}
-        className={`flex cursor-pointer ${isCollapsed ? "sm:flex" : ""}`}
+        className={`flex  cursor-pointer ${isCollapsed ? "sm:flex" : ""}`}
       >
-        <SectionTitle className="mb-2" text="Browse by Language" />
+        <SectionTitle text="Browse by Language" />
         <FontAwesomeIcon
           icon={faChevronDown}
           className={`mx-2 mt-[3px] transform text-secondary transition-transform ${
             isCollapsed ? "rotate-0" : "rotate-180"
           } animate-fade-in duration-300 ease-in-out md:hidden`}
         />
+        {activeTagId && isCollapsed ? <ActiveTagButton data={activeTagId}/>  : null}
       </div>
       <div
         className={`transition-max-height overflow-hidden duration-300 ease-in-out ${
--- components/SectionTitle.tsx
+++ components/SectionTitle.tsx
@@ -5,6 +5,7 @@ type SectionTitleProps = {
 
 export const SectionTitle = ({ text, className }: SectionTitleProps) => (
   <h3 className={`${className || ""} text-sm font-bold uppercase tracking-wider text-secondary`}>
-    {text}
+    {/* Prevents text from wrapping to the next line. */}
+    <span className="truncate">{text}</span>
   </h3>
 );
