b544b0acdd78381855069b1ef569ec0e262f395e
Adarsh Jha <132337675+adarsh-jha-dev@users.noreply.github.com>
2023-10-15 09:10:08 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,14 @@
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
-import type { AppProps } from "next/app";
+import { AppProps } from "next/app";
 import { Inter } from "next/font/google";
 import Head from "next/head";
 import { useRouter } from "next/router";
-
 import { Layout } from "../components/Layout";
 import { SponsorsBar } from "../components/SponsorsBar";
 import { AppDataProvider } from "../context/AppDataContext";
-import "../styles/globals.css";
 import NotFound from "./404";
+import "../styles/globals.css";
 
 // Fontawesome and TailwindCSS related settings
 config.autoAddCss = false;
