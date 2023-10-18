5592b083761488380c00c273a94d2dedc35cefcc
Adarsh Jha <132337675+adarsh-jha-dev@users.noreply.github.com>
2023-10-18 07:05:14 +0530

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function MyDocument() {
   return (
