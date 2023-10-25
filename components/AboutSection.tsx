8ea9cde594451d19de9dc9ec231fab028552946c
Luca Cavallin <lucavallin@users.noreply.github.com>
2023-10-26 01:38:18 +0200

    update title

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -8,12 +8,13 @@ export const AboutSection = () => {
   return (
     <div>
       <SectionTitle className="mb-2" text="About" />
+      <h1 className="hidden text-lg font-bold">verto.sh</h1>
       <p className="text-silver-500">
-        <h1 className="inline text-lg font-bold">verto.sh</h1> simplifies your entry into the
-        open-source world by curating accessible projects. Ideal for beginners making their first
-        contribution, founders building teams, and maintainers seeking new contributors.
-        <h2 className="mt-2">Your Gateway to Open-Source Collaboration!</h2>
+        <strong>verto.sh</strong> simplifies your entry into the open-source world by curating
+        accessible projects. Ideal for beginners making their first contribution, founders building
+        teams, and maintainers seeking new contributors.
       </p>
+      <h2 className="mt-2">Your Gateway to Open-Source Collaboration!</h2>
       <div className="pt-6 text-sm">
         <Link className="flex flex-row items-center" target="_blank" href="https://www.lucavall.in">
           <FontAwesomeIcon icon={faHeart} />
