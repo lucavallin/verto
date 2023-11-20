29ed63fa05c94a859eebd8d8c11c7114ab85c607
Siddharth Biswas <123821746+ATLAS2002@users.noreply.github.com>
2023-11-21 03:21:18 +0530

    Renaming ui/ to UI/

---
+++ components/UI/Divider.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable tailwindcss/no-contradicting-classname */
+export default function Divider() {
+  return (
+    <div className="w-full bg-gradient-to-b from-black-400 from-[49%] via-silver-300 via-50% to-black-400 to-[51%] p-4 text-center text-gray-400">
+      <span className="h-fit w-fit select-none bg-black-400 px-3">or</span>
+    </div>
+  );
+}
