f715f58c779dad6673dfe6ae33f19bd9f00d15ac
Luca Cavallin <lucavallin@users.noreply.github.com>
2024-07-09 12:31:26 +0200

    run linter

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -1,9 +1,9 @@
 import { faHeart } from "@fortawesome/free-regular-svg-icons";
+import { faPaintRoller } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 import config from "../app/config.mts";
 import { SectionTitle } from "./SectionTitle";
-import { faPaintRoller } from "@fortawesome/free-solid-svg-icons";
 
 export const AboutSection = () => {
   return (
